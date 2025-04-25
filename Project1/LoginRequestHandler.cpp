e348daf8d0648ca0ef85befc7521fc4a24c24f2e
maor74 <maordabool@gmail.com>
2025-04-26 00:44:53 +0300

    fixed . instead of ->

--- Project1/LoginRequestHandler.cpp
+++ Project1/LoginRequestHandler.cpp
@@ -1,5 +1,7 @@
 #include "LoginRequestHandler.h"
-
+#include "JsonRequestPacketDeserializer.h"
+#include "JsonResponsePacketSerializer.h"
+#include "LoginManager.h"
 #define LOGIN_REQUEST 1
 #define SIGNUP_REQUEST 2
 
@@ -15,35 +17,85 @@ bool LoginRequestHandler::isRequestRelevant(RequestInfo request)
 
 RequestResult LoginRequestHandler::handleRequest(RequestInfo request)
 {
-    RequestResult result;
-
     if (!isRequestRelevant(request))
     {
-        result.response = { 0xFF };  // Error code
-        result.newHandler = this;
-        return result;
+        return { { 0xFF }, this };
     }
 
     if (request.id == LOGIN_REQUEST)
     {
-        result.response = { 0x01 };  // Login success
-        result.newHandler = this;
+        return login(request);
     }
     else if (request.id == SIGNUP_REQUEST)
     {
-        result.response = { 0x02 };  // Signup success
-        result.newHandler = this;
+        return signup(request);
     }
 
-    return result;
+    return { { 0xFF }, this };
 }
 
 RequestResult LoginRequestHandler::login(RequestInfo request)
 {
-    return { { 0x01 }, this }; 
+    JsonRequestPacketDeserializer deserializer;
+    LoginRequest loginData = deserializer.deserializeLoginRequest(request.buffer);
+
+    LoginStatus status = m_handlerFactory.getLoginManager().login(loginData.username, loginData.password);
+
+    LoginResponse response;
+    RequestResult result;
+
+    switch (status)
+    {
+    case LoginStatus::SUCCESS:
+        response.status = 1;
+        result.newHandler = this;
+        break;
+    case LoginStatus::USER_NOT_FOUND:
+        response.status = 2;
+        result.newHandler = this;
+        break;
+    case LoginStatus::WRONG_PASSWORD:
+        response.status = 3;
+        result.newHandler = this;
+        break;
+    case LoginStatus::ALREADY_LOGGED_IN:
+        response.status = 4;
+        result.newHandler = this;
+        break;
+    }
+
+    JsonResponsePacketSerializer serializer;
+    result.response = serializer.serializeLoginResponse(response);
+    return result;
 }
 
 RequestResult LoginRequestHandler::signup(RequestInfo request)
 {
-    return { { 0x02 }, this };
+    JsonRequestPacketDeserializer deserializer;
+    SignupRequest signupData = deserializer.deserializeSignUpRequest(request.buffer);
+
+    SignUpStatus status = m_handlerFactory.getLoginManager().signup(signupData.username, signupData.password, signupData.email);
+
+    SignupResponse response;
+    RequestResult result;
+
+    switch (status)
+    {
+    case SignUpStatus::SUCCESS:
+        response.status = 1;
+        result.newHandler = this;
+        break;
+    case SignUpStatus::USER_ALREADY_EXISTS:
+        response.status = 2;
+        result.newHandler = this;
+        break;
+    case SignUpStatus::ERROR:
+        response.status = 3;
+        result.newHandler = nullptr;
+        break;
+    }
+
+    JsonResponsePacketSerializer serializer;
+    result.response = serializer.serializeSignUpResponse(response);
+    return result;
 }
