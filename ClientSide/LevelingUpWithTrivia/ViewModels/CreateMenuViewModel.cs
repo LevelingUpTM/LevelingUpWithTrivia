861196a7e38bd15a4848f70c30740a10696ec080
maor74 <maordabool@gmail.com>
2025-05-25 00:51:08 +0300

    added waiting room screen and logic

--- ClientSide/LevelingUpWithTrivia/ViewModels/CreateMenuViewModel.cs
+++ ClientSide/LevelingUpWithTrivia/ViewModels/CreateMenuViewModel.cs
@@ -3,14 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using LevelingUpWithTrivia.Source.Packets.Requests;
+using LevelingUpWithTrivia.Source.Packets.Responses;
+using LevelingUpWithTrivia.Source;
 using LevelingUpWithTrivia.Views;
 
 namespace LevelingUpWithTrivia.ViewModels
 {
     public partial class CreateMenuViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string roomName;
+
+        [ObservableProperty]
+        private int maxPlayers;
+
+        [ObservableProperty]
+        private int questionCount;
+
+        [ObservableProperty]
+        private int timePerQuestion;
+
+        [RelayCommand]
+        public void CreateRoom()
+        {
+            var request = new CreateRoomRequest(
+                RoomName,
+                MaxPlayers,
+                QuestionCount,
+                TimePerQuestion
+            );
+
+            Communicator.Instance.Send(request);
+            var response = Communicator.Instance.Receive();
+
+            if (response is CreateRoomResponse createRoomResponse && createRoomResponse.Status == 1)
+            {
+                // success
+                MessageBox.Show("Room created successfully!");
+                MainWindowViewModel.Current.Content = new MainMenuView();
+            }
+            else
+            {
+                MessageBox.Show("Failed to create room.");
+            }
+        }
         [RelayCommand]
         private void NavToMain()
         {
---
+++ ClientSide/LevelingUpWithTrivia/ViewModels/WaitingRoomViewModel.cs
@@ -0,0 +1,64 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LevelingUpWithTrivia.Source;
+using LevelingUpWithTrivia.Source.Packets.Requests;
+using LevelingUpWithTrivia.Source.Packets.Responses;
+using LevelingUpWithTrivia.Views;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace LevelingUpWithTrivia.ViewModels
+{
+    public partial class WaitingRoomViewModel : ObservableObject
+    {
+        private readonly int _roomId;
+        private readonly bool _isHost;
+
+        [ObservableProperty]
+        private string roomName;
+
+        [ObservableProperty]
+        private ObservableCollection<string> players = new();
+
+        public bool IsHost => _isHost;
+
+        public WaitingRoomViewModel(int roomId, string roomName, bool isHost)
+        {
+            _roomId = roomId;
+            RoomName = roomName;
+            _isHost = isHost;
+
+            RefreshPlayers();
+        }
+
+        private void RefreshPlayers()
+        {
+            Communicator.Instance.Send(new GetPlayersInRoomRequest(_roomId));
+            var response = Communicator.Instance.Receive();
+
+            if (response is GetPlayersInRoomResponse playersResponse)
+            {
+                Players.Clear();
+                foreach (var player in playersResponse.players)
+                    Players.Add(player);
+            }
+        }
+
+        [RelayCommand]
+        private void StartGame()
+        {
+            if (!_isHost)
+                return;
+
+            // TODO: Add start game request logic here
+            MessageBox.Show("Game started!");
+        }
+
+        [RelayCommand]
+        private void LeaveRoom()
+        {
+            // Optional: send a leave-room request
+            MainWindowViewModel.Current.Content = new MainMenuView();
+        }
+    }
+}
